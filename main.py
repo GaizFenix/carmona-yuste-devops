d41cbf218beb37e0557b7f30f443641b1881f47c
Gaizka Carmona <67479691+GaizFenix@users.noreply.github.com>
2025-11-11 16:19:32 +0100

    Rename sum function to my_sum and add docstring
    Renamed the sum function to my_sum and added a docstring.

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
-def sum(x, y):
-  return x + y
+"""Function to sum 2 numbers"""
+
+def my_sum(a, b):
+    return a + b
 
 if __name__ == "__main__":
-  x = 5
-  y = 3
-  print(sum(x, y))
+    x = 5
+    y = 3
+    print(sum(x, y))
